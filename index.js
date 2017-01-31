1944d2d66d901af76ded4ae377d2564bae5dcec2
Khalid Rehioui <rehioui@gmail.com>
2017-01-31 23:54:55 +0100

    Create proof of concept

---
+++ index.js
@@ -0,0 +1,62 @@
+var Arget = function (args) {
+  this._args = args;
+};
+
+Arget.prototype.first = function (type) {
+  if(type) return this.get(0, type);
+
+  return this._args[0];
+};
+
+Arget.prototype.last = function (type) {
+  if(type) return this.getRight(0, type);
+
+  return this._args[this._args.length - 1];
+};
+
+Arget.prototype.get = function (index, type) {
+  if(!type) return this._args[index];
+
+  for(var i = 0; i<this.length; i++) {
+    if(Object.getPrototypeOf(this._args[i]) == type.prototype) {
+      return this._args[i];
+    }
+  }
+};
+
+Arget.prototype.getRight = function (index, type) {
+  if(!type) return this._args[this.length - index - 1];
+
+  for(var i = this.length  - 1; i >= 0; i--) {
+    if(Object.getPrototypeOf(this._args[i]) == type.prototype) {
+      return this._args[i];
+    }
+  }
+};
+
+Arget.prototype.all = function (type) {
+  if(!type) return this.toArray();
+
+  var args = [];
+  for(var i = 0; i<this.length; i++) {
+    if(Object.getPrototypeOf(this._args[i]) == type.prototype) {
+      args.push(this._args[i]);
+    }
+  }
+
+  return args;
+};
+
+Arget.prototype.toArray = function () {
+  return Array.prototype.slice.call(this._args);
+};
+
+Object.defineProperty(Arget.prototype, 'length', { 
+  get : function () {
+    return this._args.length;
+  }
+});
+
+module.exports = function arget (args) {
+  return new Arget(args || arget.caller.arguments);
+};
