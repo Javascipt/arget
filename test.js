12101e8336b349c4f9967cbc82a3aa4e1c52fdae
Khalid Rehioui <rehioui@gmail.com>
2017-02-03 22:28:19 +0100

    Add Arget.prototype.get tests

--- test.js
+++ test.js
@@ -19,7 +19,17 @@ test('Arget.prototype.last', t => {
   t.deepEqual(genArg().last(), undefined);
 });
 
-test.todo('Arget.prototype.get');
+test('Arget.prototype.get', t => {
+  var f = function () {};
+  t.deepEqual(genArg(1, 2, 3, 4).get(0), 1);
+  t.deepEqual(genArg(2, 3, 4, 5).get(6), undefined);
+  t.deepEqual(genArg(2, 3, 4, 5).get(0, Number), 2);
+  t.deepEqual(genArg(2, 3, 4, 5).get(0, Function), undefined);
+  t.deepEqual(genArg(2, 3, 4, f).get(0, Function), f);
+  t.deepEqual(genArg(2, ()=>{}, 4, f).get(1, Function), f);
+  t.deepEqual(genArg(2, ()=>{}, 4, {}, f).get(1, Object), undefined);
+});
+
 test.todo('Arget.prototype.getRight');
 test.todo('Arget.prototype.all');
 test.todo('Arget.prototype.toArray');
